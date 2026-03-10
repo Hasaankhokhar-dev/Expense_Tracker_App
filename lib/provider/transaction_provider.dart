e5c67e55b9bd838052595ac748206270589d3bb5
Your Name <you@example.com>
2026-03-10 23:46:24 +0500

    Add transaction provider

---
+++ lib/provider/transaction_provider.dart
@@ -0,0 +1,40 @@
+import 'package:expense_tracker/database/db_helper.dart';
+import 'package:expense_tracker/models/transaction_model.dart';
+import 'package:flutter/cupertino.dart';
+
+class TransactionProvider extends ChangeNotifier {
+  final DbHelper _dbHelper = DbHelper();
+  List<TransactionModel> _transactions = [];
+  List<TransactionModel> get transactions => _transactions;
+
+  double get totalIncome {
+    return _transactions
+        .where((t) => t.type == 'income')
+        .fold(0.0, (sum, t) => sum + t.ammount);
+  }
+
+  double get totalExpence {
+    return _transactions
+        .where((t) => t.type == "expense")
+        .fold(0.0, (sum, t) => sum + t.ammount);
+  }
+
+  double get balance {
+    return totalIncome - totalExpence;
+  }
+
+  Future<void> loadTransactions() async {
+    _transactions = await _dbHelper.getAllTransactions();
+    notifyListeners();
+  }
+
+  Future<void> addTransaction(TransactionModel transaction) async {
+    await _dbHelper.insertTransaction(transaction);
+    await loadTransactions();
+  }
+
+  Future<void> deleteTransaction(int id) async {
+    await _dbHelper.deleteTransaction(id);
+    await loadTransactions();
+  }
+}
